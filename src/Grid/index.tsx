bde50824dbc84653a4e1c7d032c6dde8749ec7b8
Andres Babativa <carlos3k11@gmail.com>
2024-02-07 15:35:26 -0500

    feat: implement the Grid component

---
+++ src/Grid/index.tsx
@@ -0,0 +1,70 @@
+import {
+  AlignContent,
+  AlignItems,
+  AutoFlow,
+  Height,
+  JustifyContent,
+  JustifyItems,
+  Width,
+} from "./props";
+
+import { StyledGrid } from "./styles";
+
+export interface IGridProps {
+  templateColumns?: string;
+  templateRows?: string;
+  gap?: string;
+  justifyItems?: JustifyItems;
+  alignItems?: AlignItems;
+  justifyContent?: JustifyContent;
+  alignContent?: AlignContent;
+  autoColumns?: string;
+  autoRows?: string;
+  autoFlow?: AutoFlow;
+  margin?: string;
+  padding?: string;
+  height?: Height;
+  width?: Width;
+  children: React.ReactNode;
+}
+
+export const Grid = (props: IGridProps) => {
+  const {
+    templateColumns = "auto",
+    templateRows = "auto",
+    gap = "s0",
+    justifyItems = "stretch",
+    alignItems = "stretch",
+    justifyContent = "start",
+    alignContent = "start",
+    autoColumns = "0px",
+    autoRows = "0px",
+    autoFlow = "row",
+    margin = "s0",
+    padding = "s0",
+    height = "auto",
+    width = "auto",
+    children,
+  } = props;
+
+  return (
+    <StyledGrid
+      $templateColumns={templateColumns}
+      $templateRows={templateRows}
+      $gap={gap}
+      $justifyItems={justifyItems}
+      $alignItems={alignItems}
+      $justifyContent={justifyContent}
+      $alignContent={alignContent}
+      $autoColumns={autoColumns}
+      $autoRows={autoRows}
+      $autoFlow={autoFlow}
+      $margin={margin}
+      $padding={padding}
+      $height={height}
+      $width={width}
+    >
+      {children}
+    </StyledGrid>
+  );
+};
---
+++ src/Grid/stories/stories.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { inube } from "@inubekit/foundations";
+
+export const StyledSquare = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${inube.color.surface.gray.regular};
+  border-radius: 4px;
+  padding: 3px;
+  height: 50px;
+  width: 100%;
+  color: ${inube.color.text.light.regular};
+`;
